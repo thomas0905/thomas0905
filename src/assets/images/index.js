d09f833239b0fd2f861565cc768d907eaafc3a0c
thomaswahyu <cahyawahyu@gmail.com>
2022-01-24 09:17:18 +0700

    menambahkan ilustrasi

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,4 @@
 import Logo from './Logo.svg';
+import Ilustrasi from './Ilustrasi.svg';
 
-export {Logo};
+export {Logo, Ilustrasi};
--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -1,12 +1,16 @@
 import {Text, StyleSheet, View} from 'react-native';
 import React, {Component} from 'react';
-import {Logo, Ilustration} from '../../assets';
+import {Logo, Ilustrasi} from '../../assets';
 
 export default class Splash extends Component {
   render() {
     return (
       <View style={styles.pages}>
-        <Logo/>
+        <Logo />
+        <View style={styles.ilustrasi}>
+          <Ilustrasi />
+        </View>
+
         {/* <Ilustration/> */}
       </View>
     );
@@ -14,5 +18,6 @@ export default class Splash extends Component {
 }
 
 const styles = StyleSheet.create({
-  pages:{flex: 1, alignItems: 'center', justifyContent: 'center'}
+  pages: {flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white'},
+  ilustrasi: {position: 'absolute', bottom: 0, right: 0},
 });
